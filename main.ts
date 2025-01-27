9fd0dbc5a326eed5c859960a487746ef92cebd4c
Dominik Comerzan <dominik.comerzan.024@pslib.cz>
2025-01-27 10:31:06 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,34 @@
 radio.setGroup(53)
 
+let cas1 = 0
+let cas2 = 0
 Sensors.SetLightLevel()
 
-input.onButtonPressed(Button.A, function() {
+radio.onReceivedNumber(function (receivedNumber: number) {
+    music.playTone(440, 400)
+    if(receivedNumber === 1) {
+        cas1 = control.millis()
+    }
+    
+    if (receivedNumber === 0) {
+        basic.showString("N")
+        basic.clearScreen()
+        control.reset()
+    }
+})
+
+
+input.onButtonPressed(Button.AB, function () {
     Sensors.SetLightLevel()
 })
 
-Sensors.OnLightDrop(function() {
-    
-})
-radio.onReceivedNumber(function(receivedNumber: number) {
-    
+Sensors.OnLightDrop(function () {
+    cas2 = control.millis()
+    music.playTone(520, 400)
+    radio.sendNumber((cas2 - cas1) / 1000)
+    basic.showNumber((cas2 - cas1)/1000)
+    basic.pause(10000)
+    control.reset()
 })
 
-radio.sendNumber(0)
+
