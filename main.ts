c7144e3a6a011485cff3af935f6fdf5c883f2b8d
Dominik Comerzan <dominik.comerzan.024@pslib.cz>
2025-02-10 09:21:46 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,41 @@
 radio.setGroup(53)
 
+let dobehl = false
 let cas1 = 0
 let cas2 = 0
 Sensors.SetLightLevel()
 
 radio.onReceivedNumber(function (receivedNumber: number) {
-    music.playTone(440, 400)
     if(receivedNumber === 1) {
+        music.playTone(440, 400)
         cas1 = control.millis()
     }
     
     if (receivedNumber === 0) {
         basic.showString("N")
         basic.clearScreen()
-        control.reset()
+        Sensors.SetLightLevel()
     }
 })
 
+input.onButtonPressed(Button.B, function() {
+    dobehl = false
+    cas1 = 0
+    cas2 = 0
+})
 
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.A, function () {
     Sensors.SetLightLevel()
 })
 
 Sensors.OnLightDrop(function () {
-    cas2 = control.millis()
-    music.playTone(520, 400)
-    radio.sendNumber((cas2 - cas1) / 1000)
-    basic.showNumber((cas2 - cas1)/1000)
-    basic.pause(10000)
-    control.reset()
+    if(dobehl === false) {
+        dobehl = true
+        cas2 = control.millis()
+        music.playTone(520, 400)
+        radio.sendNumber((cas2 - cas1) / 1000)
+        basic.showNumber((cas2 - cas1) / 1000)
+    }
 })
 
 
